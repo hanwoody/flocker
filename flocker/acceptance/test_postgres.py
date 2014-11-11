ee82e3f25745424e3b1afe0ed831afc7f9130472
Adam Dangoor <adamdangoor@gmail.com>
2014-11-11 14:33:34 +0000

    Get rid of sleep - loop until postgres is ready

--- flocker/acceptance/test_postgres.py
+++ flocker/acceptance/test_postgres.py
@@ -103,6 +103,40 @@ class PostgresTests(TestCase):
 
         return d
 
+    def _get_postgres_client(self, host, user, port, database=None):
+        """
+        Returns a ``Deferred`` which fires with a ``MongoClient`` when one has been
+        created.
+
+        See http://api.mongodb.org/python/current/api/pymongo/mongo_client.html#
+            pymongo.mongo_client.MongoClient
+        for more parameter information.
+
+        :param bytes host: Hostname or IP address of the instance to connect to.
+        :param int port: Port number on which to connect.
+
+        The tutorial says "If you get a connection refused error try again after a
+        few seconds; the application might take some time to fully start up."
+        and so here we wait until the client can be created.
+
+        # TODO document this properly
+        # TODO use **kwargs?
+        """
+        from flocker.testtools import loop_until
+        def create_mongo_client():
+            from psycopg2 import OperationalError
+            try:
+                if database is None:
+                    return connect(host=host, user=user, port=port)
+                else:
+                    return connect(host=host, user=user, port=port,
+                        database=database)
+            except OperationalError:
+                return False
+
+        d = loop_until(create_mongo_client)
+        return d
+
     def test_postgres(self):
         """
         PostgreSQL and its data can be deployed and moved with Flocker. In
@@ -119,51 +153,66 @@ class PostgresTests(TestCase):
         column = b'testcolumn'
         data = 3
 
-        from time import sleep
-        # TODO get rid of this sleep
-        sleep(5)
-        with connect(host=self.node_1, user=user,
-            port=POSTGRES_EXTERNAL_PORT) as connection_to_application:
+        connecting_to_application = self._get_postgres_client(host=self.node_1,
+            user=user, port=POSTGRES_EXTERNAL_PORT)
+
+        def create_database(connection_to_application):
             connection_to_application.autocommit = True
             with connection_to_application.cursor() as application_cursor:
                 application_cursor.execute("CREATE DATABASE " + database + ";")
 
-        with connect(host=self.node_1, user=user, port=POSTGRES_EXTERNAL_PORT,
-                     database=database) as db_connection_node_1:
-            with db_connection_node_1.cursor() as db_node_1_cursor:
-                db_node_1_cursor.execute("CREATE TABLE " + table + " (" +
-                    column + " int);")
-                db_node_1_cursor.execute("INSERT INTO " + table + " (" +
-                    column + ") VALUES (%(data)s);", {'data': data})
-                db_node_1_cursor.execute("SELECT * FROM " + table + ";")
-                db_connection_node_1.commit()
-                self.assertEqual(db_node_1_cursor.fetchone()[0], data)
+        connecting_to_application.addCallback(create_database)
 
-        postgres_deployment_moved = {
-            u"version": 1,
-            u"nodes": {
-                self.node_1: [],
-                self.node_2: [POSTGRES_APPLICATION],
-            },
-        }
+        getting_database = connecting_to_application.addCallback(
+            lambda _: self._get_postgres_client(host=self.node_1, user=user,
+                port=POSTGRES_EXTERNAL_PORT, database=database)
+        )
 
-        flocker_deploy(self, postgres_deployment_moved,
-                       self.postgres_application)
+        def insert_data(connection_to_db):
+            with connection_to_db as db_connection_node_1:
+                with db_connection_node_1.cursor() as db_node_1_cursor:
+                    db_node_1_cursor.execute("CREATE TABLE " + table + " (" +
+                        column + " int);")
+                    db_node_1_cursor.execute("INSERT INTO " + table + " (" +
+                        column + ") VALUES (%(data)s);", {'data': data})
+                    db_node_1_cursor.execute("SELECT * FROM " + table + ";")
+                    db_connection_node_1.commit()
+                    self.assertEqual(db_node_1_cursor.fetchone()[0], data)
 
-        verifying_deployment = assert_expected_deployment(self, {
-            self.node_1: set([]),
-            self.node_2: set([POSTGRES_UNIT]),
-        })
+        inserting_data = getting_database.addCallback(insert_data)
 
-        def verify_data_moves(client_1):
-            # TODO get rid of this sleep
-            sleep(5)
-            with connect(host=self.node_2, user=user,
-                         port=POSTGRES_EXTERNAL_PORT,
-                         database=database) as db_connection_node_2:
+        def move_postgres(ignored):
+            postgres_deployment_moved = {
+                u"version": 1,
+                u"nodes": {
+                    self.node_1: [],
+                    self.node_2: [POSTGRES_APPLICATION],
+                },
+            }
+
+            flocker_deploy(self, postgres_deployment_moved,
+                           self.postgres_application)
+
+            verifying_deployment = assert_expected_deployment(self, {
+                self.node_1: set([]),
+                self.node_2: set([POSTGRES_UNIT]),
+            })
+
+            return verifying_deployment
+
+        moving_postgres = inserting_data.addCallback(move_postgres)
+
+        getting_database_node_2 = moving_postgres.addCallback(
+            lambda _: self._get_postgres_client(host=self.node_2, user=user,
+                port=POSTGRES_EXTERNAL_PORT, database=database)
+        )
+
+        def verify_data_moves(connection_to_db):
+            with connection_to_db as db_connection_node_2:
                 with db_connection_node_2.cursor() as db_node_2_cursor:
                     db_node_2_cursor.execute("SELECT * FROM " + table + ";")
                     self.assertEqual(db_node_2_cursor.fetchone()[0], data)
 
-        verifying = verifying_deployment.addCallback(verify_data_moves)
-        return verifying
+        verifying_data_moves = getting_database_node_2.addCallback(
+            verify_data_moves)
+        return verifying_data_moves
