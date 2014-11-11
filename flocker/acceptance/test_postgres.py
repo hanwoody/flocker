d5458a9683198cfb730835e37a7826c63667e4d2
Adam Dangoor <adamdangoor@gmail.com>
2014-11-11 09:45:16 +0000

    Started separating procedure into multiple tests

--- flocker/acceptance/test_postgres.py
+++ flocker/acceptance/test_postgres.py
@@ -16,6 +16,24 @@ from .testtools import (assert_expected_deployment, flocker_deploy, get_nodes,
 # add to the licensing google doc
 import psycopg2
 
+internal_port = 5432
+external_port = 5432
+
+POSTGRES_UNIT = Unit(
+    name=u"postgres-volume-example",
+    container_name=BASE_NAMESPACE + u"postgres-volume-example",
+    activation_state=u'active',
+    container_image=u"postgres" + u':latest',
+    ports=frozenset([
+        PortMap(internal_port=internal_port,
+                external_port=external_port)
+        ]),
+    volumes=frozenset([
+        Volume(node_path=FilePath(b'/tmp'),
+           container_path=FilePath(u'/var/lib/postgresql/data')),
+        ]),
+)
+
 class PostgresTests(TestCase):
     """
     Tests for running and managing PostgreSQL with Flocker.
@@ -26,25 +44,19 @@ class PostgresTests(TestCase):
     # TODO Link to this file from postgres.rst
     """
     @require_flocker_cli
-    def test_postgres(self):
-        """
-        PostgreSQL and its data can be deployed and moved with FLocker.
-        """
+    def setUp(self):
         getting_nodes = get_nodes(num_nodes=2)
 
         def deploy(node_ips):
-            node_1, node_2 = node_ips
+            self.node_1, self.node_2 = node_ips
 
             postgres_deployment = {
                 u"version": 1,
                 u"nodes": {
-                    node_1: [u"postgres-volume-example"],
-                    node_2: [],
+                    self.node_1: [u"postgres-volume-example"],
+                    self.node_2: [],
                 },
             }
-
-            internal_port = 5432
-            external_port = 5432
 
             postgres_application = {
                 u"version": 1,
@@ -68,66 +80,60 @@ class PostgresTests(TestCase):
 
             flocker_deploy(self, postgres_deployment, postgres_application)
 
-            ports = frozenset([
-                PortMap(internal_port=internal_port,
-                        external_port=external_port)
-            ])
+        getting_nodes.addCallback(deploy)
+        return getting_nodes
 
-            volumes = frozenset([
-                Volume(node_path=FilePath(b'/tmp'),
-                       container_path=FilePath(u'/var/lib/postgresql/data')),
-            ])
+    def test_deploy(self):
+        # TODO docstrings
+        d = assert_expected_deployment(self, {
+            self.node_1: set([POSTGRES_UNIT]),
+            self.node_2: set([]),
+        })
 
-            unit = Unit(
-                name=u"postgres-volume-example",
-                container_name=BASE_NAMESPACE + u"postgres-volume-example",
-                activation_state=u'active',
-                container_image=u"postgres" + u':latest',
-                ports=ports,
-                volumes=volumes,
-            )
+        return d
+
+    def test_postgres(self):
+        """
+        PostgreSQL and its data can be deployed and moved with FLocker.
+        """
+
             # psql postgres --host 172.16.255.250 --port 5432 --username postgres
-            from time import sleep
-            # TODO get rid of this sleep
-            sleep(5)
+        from time import sleep
+        # TODO get rid of this sleep
+        sleep(5)
 
-            # TODO bytes or unicode (for this and filepaths?)
-            conn = psycopg2.connect(host=node_1, user=u'postgres', port=external_port)
-            conn.autocommit = True
-            cur = conn.cursor()
-            cur.execute("CREATE DATABASE flockertest;")
-            cur.close()
-            conn.close()
+        # TODO bytes or unicode (for this and filepaths?)
+        conn = psycopg2.connect(host=self.node_1, user=u'postgres', port=external_port)
+        conn.autocommit = True
+        cur = conn.cursor()
+        cur.execute("CREATE DATABASE flockertest;")
+        cur.close()
+        conn.close()
 
 
-            conn = psycopg2.connect(host=node_1, user=u'postgres', port=external_port, database='flockertest')
-            cur = conn.cursor()
-            cur.execute("CREATE TABLE testtable (testcolumn int);")
-            cur.execute("INSERT INTO testtable (testcolumn) VALUES (3);")
-            cur.execute("SELECT * FROM testtable;")
-            conn.commit()
-            self.assertEqual(cur.fetchone(), (3,))
-            # TODO put closes in cleanup
+        conn = psycopg2.connect(host=self.node_1, user=u'postgres', port=external_port, database='flockertest')
+        cur = conn.cursor()
+        # TODO use named arguments
+        cur.execute("CREATE TABLE testtable (testcolumn int);")
+        cur.execute("INSERT INTO testtable (testcolumn) VALUES (3);")
+        cur.execute("SELECT * FROM testtable;")
+        conn.commit()
+        self.assertEqual(cur.fetchone(), (3,))
+        # TODO put closes in cleanup
 
-            cur.close()
-            conn.close()
+        cur.close()
+        conn.close()
 
-            postgres_deployment_moved = {
-                u"version": 1,
-                u"nodes": {
-                    node_1: [],
-                    node_2: [u"postgres-volume-example"],
-                },
-            }
+        postgres_deployment_moved = {
+            u"version": 1,
+            u"nodes": {
+                self.node_1: [],
+                self.node_2: [u"postgres-volume-example"],
+            },
+        }
 
             #flocker_deploy(self, postgres_deployment_moved, postgres_application)
 
-            d = assert_expected_deployment(self, {
-                node_1: set([unit]),
-                node_2: set([]),
-            })
 
-            return d
 
-        getting_nodes.addCallback(deploy)
-        return getting_nodes
+        
